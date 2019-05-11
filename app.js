678b37da409a9d3cb3d42d1dc873ecf79a93ceac
Utkarsh <srivastavautkarsh8097@gmail.com>
2019-05-11 20:32:40 +0530

    Removed compile-run dependency && added glot.io API

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const { cpp, c, python, node } = require('compile-run');
+// const { cpp, c, python, node } = require('compile-run');
+const evaluate = require('./eval');
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -12,32 +13,18 @@ app.use(express.static(path.join(__dirname, 'client', 'build')));
 // ======================================================
 
 app.post('/eval', (req, res) => {
-    let func;
     const code = req.body.code;
-    const params = req.body.inputs;
-    if (req.body.language === 'cpp') {
-        func = cpp;
-    }
-    else if (req.body.language === 'python') {
-        func = python;
-    }
-    else if (req.body.language === 'c') {
-        func = c;
-    }
-    else if (req.body.language === 'javascript') {
-        func = node;
-    }
-    func.runSource(code, { stdin: params })
-        .then(data => {
+    const input = req.body.input;
+    const language = req.body.language;
+    evaluate(language, code, input, (err, data) => {
+        if (err)
+            res.status(500).send({ "msg": "Some error occured" });
+        else {
             console.log(data);
-            if (!data.stderr) {
-                res.send(data.stdout);
-            }
-            else {
-                res.status(200).send(data.stderr);
-            }
-        })
-        .catch(err => res.status(500).send(err));
+            if (!data.stderr) res.send(data.stdout);
+            else res.status(200).send(data.stderr);
+        }
+    })
 });
 
 app.get('/*', function (req, res) {
---
+++ eval.js
@@ -0,0 +1,56 @@
+const request = require('request');
+const crypto = require('crypto');
+
+const fileExtension = language => {
+    if (language === 'c')
+        return '.c';
+    else if (language === 'cpp')
+        return '.cpp';
+    else if (language === 'javascript')
+        return '.js';
+    else if (language === 'python')
+        return '.py';
+}
+
+const generateFilename = language => {
+    return crypto.createHash('md5') + fileExtension(language);
+}
+
+const evaluate = (language, code, input, cb) => {
+    const req = { stdin: input, "files": [{ "name": generateFilename(language), "content": code }] };
+    const config = {
+        "url": `https://run.glot.io/languages/${language}/latest`,
+        headers: {
+            "Content-type": "application/json",
+            "Authorization": "Token 73dcda85-656a-4cd3-b3ec-ec26a9820d92"
+        },
+        "body": JSON.stringify(req)
+    }
+    request.post(config, (err, httpres, body) => {
+        body = JSON.parse(body);
+        if (err) cb(err);
+        else cb(err, body);
+    });
+}
+
+module.exports = evaluate
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
